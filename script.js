89347bfe138d33d01dc659299c3ba30a50bfb841
lockedforvce <guransh09@icloud.com>
2026-03-24 20:28:28 +1100

    Update script.js

--- script.js
+++ script.js
@@ -19,13 +19,17 @@ function erf(x) {
 
 // Normal CDF
 function normalCDF(x, mean, std) {
-  return 0.5 * (1 + erf((2*x - mean) / (std * Math.sqrt(2))));
+  return 0.5 * (1 + erf((x - mean) / (std * Math.sqrt(2))));
 }
-let currentMean = null;
-let currentStd = null;
+let currentMean3 = null;
+let currentStd3 = null;
+let currentMean4 = null;
+let currentStd4 = null;
+let currentMeanX = null;
+let currentStdX = null;
 const subjects = {
-  stoopid: { meanX: null, Mean4: null, Mean3: null, sdX: null, sd4: null, sd3: null, MaxMarksX: null, MaxMarks4: null, MaxMarks3: null},
-  english: { meanX: 32.6, Mean4: 66.2, Mean3: 65.7, sdX: 8, sd4: 14.9, sd3: 14.8, MaxMarksX: 60, MaxMarks4: 100, MaxMarks3: 100},
+  stoopid: { MeanX: null, Mean4: null, Mean3: null, sdX: null, sd4: null, sd3: null, MaxMarksX: null, MaxMarks4: null, MaxMarks3: null},
+  english: { MeanX: 32.6, Mean4: 66.2, Mean3: 65.7, sdX: 8, sd4: 14.9, sd3: 14.8, MaxMarksX: 60, MaxMarks4: 100, MaxMarks3: 100},
 };
 function updateValues() {
   const selected = document.getElementById("subject").value;
@@ -47,6 +51,8 @@ function roundTo(num, decimals) {
 }
 // Button function
 function calculate() {
+  const z = parseFloat(document.getElementById("u3").value);
+  const y = parseFloat(document.getElementById("u4").value);
   const x = parseFloat(document.getElementById("x").value);
   const selected = document.getElementById("subject").value;
   const data = subjects[selected];
@@ -59,7 +65,7 @@ function calculate() {
 
   let p3 = normalCDF(z, currentMean3, currentStd3);
   let p4 = normalCDF(y, currentMean4, currentStd4);
-  let pX = normalCDF(x, currentMeanX, currentStdX);
+  let pX = normalCDF(2*x, currentMeanX, currentStdX);
  
   p3 = Math.max(0, Math.min(1, p3));
   p4 = Math.max(0, Math.min(1, p4));
@@ -67,15 +73,21 @@ function calculate() {
 
   if ((z) >= data.MaxMarks3) {
   p3 = .9999;
+  }
   if ((y) >= data.MaxMarks4) {
   p4 = .9999;
+  }
   if ((2 * x) >= data.MaxMarksX) {
   pX = .9999;
-
+  }
 }
-  let StudyScore = (p3 * .25) + (p4 * .25) + (pX * .5)
+let ss3 = p3 * 50;
+let ss4 = p4 * 50;
+let ssX = pX * 50;
+
+let StudyScore = (ss3 * 0.25) + (ss4 * 0.25) + (ssX * 0.5);
    
     document.getElementById("ss").innerText =
     "Study Score = " + roundTo(StudyScore,2);
-}
+
 
